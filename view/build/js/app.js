16ef2277a83720d8cc4bbbd226a7d68c8e9edf4a
Geovanni Vera <geovanni.vera23@gmail.com>
2024-07-12 18:24:25 -0600

    se cambio el text area a input type number en formulario inventario

--- view/build/js/app.js
+++ view/build/js/app.js
@@ -8,31 +8,21 @@ app();
 
 
 function removeRow() {
-    const btn = document.querySelectorAll('.trash');
-    console.log(btn)
-    btn.forEach(element => {
-        console.log(element);
-        element.addEventListener('click', (event) => {
-            //Añadimos una clase para que se elimine
+    document.querySelector('.contenedor__lista').addEventListener('click', (event) => {
+        if (event.target.classList.contains('trash')) {
             const contenedor = document.querySelector('.contenedor__lista');
-            console.log(contenedor);
             const p = document.createElement('p');
-                
-            event.target.parentNode.parentNode.classList.add('animation');
+            
             setTimeout(() => {
                 event.target.parentNode.parentNode.remove();
                 p.classList.add('alerta-eliminado');
                 p.textContent = "Objeto Eliminado Correctamente";
                 contenedor.insertBefore(p, contenedor.firstChild);
-            }, 2000);
-            
-
-            
-
+            }, 1000);
+    
             setTimeout(() => {
                 p.remove();
-            }, 6000);
-        })
+            }, 3000);
+        }
     });
-
 }
